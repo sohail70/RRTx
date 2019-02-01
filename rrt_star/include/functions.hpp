48d32c12c4fca81dd07670386f442e2dc50bfc99
sohail70 <sohail.e.nia@gmail.com>
2019-02-01 14:40:47 +0330

    star ing project :)

---
+++ rrt_star/include/functions.hpp
@@ -0,0 +1,11 @@
+#include "ros/ros.h"
+#include <vector>
+#include <tuple>
+#include <random>
+#include <math.h>
+#include "nav_msgs/OccupancyGrid.h"
+using namespace std;
+
+typedef vector<vector< double>> Matrix;
+typedef vector<double> Row;
+
---
+++ rrt_star/src/rrt_star.cpp
@@ -0,0 +1,29 @@
+//TODO : build the trajectory tree and plot it in rviz
+//TODO : obstacles must be checked very carefully!
+//TODO : May be I should reset planning when ever new map is recieved! what you say?!
+#include "std_msgs/String.h"
+#include <sstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include "stdint.h"
+#include "functions.hpp"
+
+#include "nav_msgs/OccupancyGrid.h"
+#include "geometry_msgs/PointStamped.h"
+#include "std_msgs/Header.h"
+#include "nav_msgs/MapMetaData.h"
+#include "geometry_msgs/Point.h"
+#include "visualization_msgs/Marker.h"
+#include <tf/transform_listener.h>
+
+int main(int argc, char **argv)
+{
+	ros::Rate rate(100);
+    ros::init(argc, argv, "optimized_rapid_random_tree");
+
+        ros::spinOnce();
+        rate.sleep();
+    
+    return 0;
+}
